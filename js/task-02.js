2e8eacd23d1fef1c5ae044998703cf9b765c3642
Al <128792416+AlinaDemchenko@users.noreply.github.com>
2023-06-05 19:55:37 -0700

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,7 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+ingredients.forEach(el => {const li = document.createElement('li'); li.textContent = el; li.classList.add('item'); document.querySelector('#ingredients').append(li);})
+

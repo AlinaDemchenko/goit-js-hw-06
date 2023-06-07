df99eba5c531aeeafdde51d1b9f7e05e9e408ac3
Al <128792416+AlinaDemchenko@users.noreply.github.com>
2023-06-06 17:22:03 -0700

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('form')
+
+const handleLogin = event => {
+event.preventDefault()
+const{elements: {email, password}} = event.currentTarget;
+if (email.value && password.value){
+    const userLogin = {email: email.value, password: password.value};
+    console.log(userLogin);
+} else{
+alert('Всі поля повинні бути заповнені!')}
+event.currentTarget.reset();
+}
+
+form.addEventListener('submit', handleLogin)
 
